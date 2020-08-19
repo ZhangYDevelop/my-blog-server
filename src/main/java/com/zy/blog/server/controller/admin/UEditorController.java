7a563dd81d4cb07cc63ac98261ec7b47d3dd6628
ZhangYDevelop <2389311433@qq.com>
2020-08-19 11:20:58 +0800

    接口调整

--- src/main/java/com/zy/blog/server/controller/admin/UEditorController.java
+++ src/main/java/com/zy/blog/server/controller/admin/UEditorController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ClassUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
